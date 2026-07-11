dd31869c4abcdc7c5262a6a395e43d2ac6fcb4b3
felipesfq <felipe.s.f.q@gmail.com>
2026-07-11 15:49:08 -0300

    Exercícios 1EST

--- exe3_cap2.py
+++
@@ -1,9 +0,0 @@
-horas = input("Digite as horas: ")
-taxa = input("Digite a taxa: ")
-
-horas = float(horas)
-taxa = float(taxa)
-
-pagamento = horas * taxa
-
-print("Valor a ser pago:", pagamento)
--- exe5_cap2.py
+++
@@ -1,7 +0,0 @@
-celsius = input("Digite a temperatura em Celsius: ")
-
-celsius = float(celsius)
-
-fahrenheit = celsius * 9 / 5 + 32
-
-print("Temperatura em Fahrenheit:", fahrenheit)
